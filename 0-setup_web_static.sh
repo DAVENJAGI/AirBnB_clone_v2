57e849db3b26082c2890e7af54f939eec2044636
DAVENJAGI <njagidave39@gmail.com>
2023-12-16 08:09:26 -0500

    task_1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#Bashscript that sets upa  web server for deploymenr of web_static Air_BnB clone
+
+sudo apt-get update
+sudo apt-get install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+	<head>
+	</head>
+	<body>
+	  Holberton School
+	</body>
+	</html>" | sudo tee /data/web_static/current/index.html
+
+sudo ln -s -f /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
