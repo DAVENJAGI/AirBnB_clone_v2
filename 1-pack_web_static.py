b26eb53edc98881284409ef6b7eca68210116cd2
DAVENJAGI <njagidave39@gmail.com>
2023-12-16 08:52:16 -0500

    task_2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+def do_pack():
+    """Script that generetes a tgz archive from content of web_static"""
+
+    filename = strftime("%y%m%d%H%M%S")
+
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "version/web_static_{}.tgz" .format(filename)
+
+    except Exception as e:
+        return None
