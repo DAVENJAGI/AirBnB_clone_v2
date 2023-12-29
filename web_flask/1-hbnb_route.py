815bcca4efd1fed249e5f6d743cd3c488d68b706
DAVENJAGI <njagidave39@gmail.com>
2023-12-29 07:22:00 -0500

    Web_framework

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that starts flask, listens on host 0.0.0.0, on port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_hbnb():
+    """returns string HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
