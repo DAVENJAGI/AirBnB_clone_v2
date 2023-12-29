8291595f19949ca6db2556ea02ca3ee7304a2871
DAVENJAGI <njagidave39@gmail.com>
2023-12-29 07:09:00 -0500

    Web_framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,13 @@ A script that starts flask, listens on host 0.0.0.0, on port 5000
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """returns Hello HBNB string"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
