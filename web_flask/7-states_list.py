f9b4f3ff16b16c647dcb42f3be63ec5e62288753
DAVENJAGI <njagidave39@gmail.com>
2023-12-31 17:50:25 -0500

    Web_Flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def list_states():
-    """Displays a HTML page inside the tag BODY"""
-    states = storage.all("State")
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage
+    States are sorted by name
+    """
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def tear_down(exc):
-    """closes connection using storage.close()"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
